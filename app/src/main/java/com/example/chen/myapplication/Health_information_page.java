0b7d11ca8052539094fff743deec4e923aa66972
chxxbb <chxxbb@outlook.com>
2016-06-17 12:53:11 +0800

    2016年6月17日 12:52:51
               新增:
               1.健康资讯详情页面

---
+++ app/src/main/java/com/example/chen/myapplication/Health_information_page.java
@@ -0,0 +1,15 @@
+package com.example.chen.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chen on 2016/6/17.
+ */
+public class Health_information_page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health_information_page);
+    }
+}
