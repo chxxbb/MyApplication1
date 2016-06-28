d0bf739a6c9865c89aeeea89b3836eea3d9a4ccf
chxxbb <chxxbb@outlook.com>
2016-06-28 10:34:52 +0800

    2016年6月28日10:34:18
               新增:
               1.患者管理页面

---
+++ app/src/main/java/com/example/chen/myapplication/Patient_management.java
@@ -0,0 +1,41 @@
+package com.example.chen.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chen on 2016/6/28.
+ */
+public class Patient_management extends Activity {
+
+    private ListView listView = null;
+    private List<String> data = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_management);
+
+        listView = (ListView) findViewById(R.id.patient_management_listview);
+
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.patient_management_item, getData()));
+
+
+    }
+
+    public List<String> getData() {
+
+        List<String> data = new ArrayList<String>();
+        data.add("尧舜禹");
+        data.add("诸葛孔明");
+        data.add("张飞");
+        data.add("关羽关二爷");
+
+        return data;
+    }
+}
