0344890dcb3ebd3cc9cce8c0efffc85cf14f9467
chxxbb <chxxbb@outlook.com>
2016-06-28 11:33:21 +0800

    2016年6月28日 11:33:17
               新增:
               1.知识讲堂页面框架,目前可以点击更换fragment内容了

---
+++ app/src/main/java/com/example/chen/myapplication/Information_activity.java
@@ -0,0 +1,81 @@
+package com.example.chen.myapplication;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Chen on 2016/6/28.
+ */
+public class Information_activity extends Activity implements View.OnClickListener {
+
+    FragmentManager mFragmentManager = null;
+
+    RelativeLayout activity_title_tag1, activity_title_tag2;
+
+    Information_school fg1, fg2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_activity);
+
+        mFragmentManager = getFragmentManager();
+
+        bindViews();
+
+        activity_title_tag1.performClick();
+
+    }
+
+    private void bindViews() {
+
+        activity_title_tag1 = (RelativeLayout) findViewById(R.id.information_activity_title_relativelayout1);
+        activity_title_tag2 = (RelativeLayout) findViewById(R.id.information_activity_title_relativelayout2);
+
+        activity_title_tag1.setOnClickListener(this);
+        activity_title_tag2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        hideAllFragment(mFragmentTransaction);
+
+        switch (v.getId()) {
+            case R.id.information_activity_title_relativelayout1:
+                activity_title_tag1.setSelected(true);
+
+                if (fg1 == null) {
+                    fg1 = new Information_school();
+                    mFragmentTransaction.add(R.id.information_activity_framelayout, fg1);
+                } else {
+                    mFragmentTransaction.show(fg1);
+                }
+                break;
+
+            case R.id.information_activity_title_relativelayout2:
+
+                activity_title_tag2.setSelected(true);
+
+                if (fg2 == null) {
+                    fg2 = new Information_school();
+                    mFragmentTransaction.add(R.id.information_activity_framelayout, fg2);
+                } else {
+                    mFragmentTransaction.show(fg2);
+                }
+                break;
+        }
+
+        mFragmentTransaction.commit();
+
+    }
+
+    private void hideAllFragment(FragmentTransaction mFragmentTransaction) {
+        if (fg1 != null) mFragmentTransaction.hide(fg1);
+        if (fg2 != null) mFragmentTransaction.hide(fg2);
+    }
+}
---
+++ app/src/main/java/com/example/chen/myapplication/Information_school.java
@@ -0,0 +1,23 @@
+package com.example.chen.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Chen on 2016/6/28.
+ */
+public class Information_school extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.information_shool, container, false);
+
+
+        return view;
+    }
+}
