f33047f6423565e7adb0d884790577ca51ac1462
chxxbb <chxxbb@outlook.com>
2016-07-08 15:57:28 +0800

    2016年7月8日 15:57:16
               增加:
               1.个人中心页面-基本资料修改页面完成

---
+++ app/src/main/java/com/example/chen/myapplication/Personal_information_changes_page.java
@@ -0,0 +1,16 @@
+package com.example.chen.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chen on 2016/7/8.
+ */
+public class Personal_information_changes_page extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_information_changes_page);
+    }
+}
--- app/src/main/java/com/example/chen/myapplication/Personal_settings.java
+++ app/src/main/java/com/example/chen/myapplication/Personal_settings.java
@@ -2,21 +2,39 @@ package com.example.chen.myapplication;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.example.chen.myapplication.utils.ContextUtil;
 
 /**
  * Created by Chen on 2016/7/7.
  */
 public class Personal_settings extends Fragment {
+
+    RelativeLayout personal_settings_basic_information_relativelayout = null;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.personal_settings, container, false);
+
+        personal_settings_basic_information_relativelayout = (RelativeLayout) view.findViewById(R.id.personal_settings_basic_information_relativelayout);
+
+        personal_settings_basic_information_relativelayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Personal_information_changes_page.class);
+                getActivity().startActivity(intent);
+            }
+        });
 
 
         return view;
