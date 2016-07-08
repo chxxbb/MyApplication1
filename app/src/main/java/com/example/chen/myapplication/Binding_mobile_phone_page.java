e43b26de243172db6932da3388623cf6977fcf98
chxxbb <chxxbb@outlook.com>
2016-07-08 17:27:30 +0800

    2016年7月8日 17:27:14
               增加:
               1.个人中心页面-基本资料修改页面-绑定手机页面完成

--- app/src/main/java/com/example/chen/myapplication/Binding_mobile_phone_page.java
+++ app/src/main/java/com/example/chen/myapplication/Binding_mobile_phone_page.java
@@ -4,15 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * Created by Chen on 2016/6/17.
+ * Created by Chen on 2016/7/8.
  */
-public class Health_information_page extends Activity {
+public class Binding_mobile_phone_page extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.health_information_page);
-
-
+        setContentView(R.layout.binding_mobile_phone_page);
     }
 }
--- app/src/main/java/com/example/chen/myapplication/Personal_information_changes_page.java
+++ app/src/main/java/com/example/chen/myapplication/Personal_information_changes_page.java
@@ -1,16 +1,32 @@
 package com.example.chen.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Chen on 2016/7/8.
  */
 public class Personal_information_changes_page extends Activity {
 
+    RelativeLayout personal_information_changes_page_phone_relativelayout = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_information_changes_page);
+
+        personal_information_changes_page_phone_relativelayout = (RelativeLayout) findViewById(R.id.personal_information_changes_page_phone_relativelayout);
+
+        personal_information_changes_page_phone_relativelayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Personal_information_changes_page.this, Binding_mobile_phone_page.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
