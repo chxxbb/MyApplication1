3591c0c28799f8f18ecf4efaa93ebb4c98386c77
chxxbb <chxxbb@outlook.com>
2016-07-09 11:32:06 +0800

    2016年7月9日 11:31:52
               增加:
               1.个人中心页面-基本资料修改页面-昵称修改页面完成

---
+++ app/src/main/java/com/example/chen/myapplication/Personal_settings_name_change_page.java
@@ -0,0 +1,84 @@
+package com.example.chen.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Chen on 2016/7/9.
+ */
+public class Personal_settings_name_change_page extends Activity {
+
+    private int editStart;
+    private int editEnd;
+    private int maxLen = 10; // the max byte
+
+    EditText personal_settings_name_change_page_edittext = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_settings_name_change_page);
+
+        personal_settings_name_change_page_edittext = (EditText) findViewById(R.id.personal_settings_name_change_page_edittext);
+
+        //获取焦点
+        personal_settings_name_change_page_edittext.setFocusable(true);
+        personal_settings_name_change_page_edittext.setFocusableInTouchMode(true);
+        personal_settings_name_change_page_edittext.requestFocus();
+
+
+        //中文限制6个字,英文限制12个
+        personal_settings_name_change_page_edittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                editStart = personal_settings_name_change_page_edittext.getSelectionStart();
+                editEnd = personal_settings_name_change_page_edittext.getSelectionEnd();
+
+                if (!TextUtils.isEmpty(personal_settings_name_change_page_edittext.getText())) {
+                    int varlength = 0;
+                    int size = 0;
+                    String etstring = personal_settings_name_change_page_edittext.getText().toString().trim();
+                    char[] ch = etstring.toCharArray();
+                    for (int i = 0; i < ch.length; i++) {
+                        size++;
+                        if (ch[i] >= 0x4e00 && ch[i] <= 0x9fbb) {
+                            varlength = varlength + 2;
+                        } else
+                            varlength++;
+                        if (varlength > 12) {
+                            break;
+                        }
+                    }
+                    if (varlength > 12) {
+                        s.delete(size - 1, etstring.length());
+                    }
+                }
+            }
+        });
+
+        //打开默认输入软键盘
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+
+
+    }
+}
