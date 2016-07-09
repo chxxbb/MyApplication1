edd733dba815a9bc29b4c88e92fa40cf0690ad3f
chxxbb <chxxbb@outlook.com>
2016-07-09 11:53:55 +0800

    2016年7月9日 11:53:46
               增加:
               1.个人中心页面-基本资料修改页面-性别修改页面完成

--- app/src/main/java/com/example/chen/myapplication/Personal_information_changes_page.java
+++ app/src/main/java/com/example/chen/myapplication/Personal_information_changes_page.java
@@ -12,6 +12,7 @@ import android.widget.RelativeLayout;
 public class Personal_information_changes_page extends Activity {
 
     RelativeLayout personal_information_changes_page_phone_relativelayout = null;
+    RelativeLayout personal_information_changes_page_name_relativelayout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,16 @@ public class Personal_information_changes_page extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Personal_information_changes_page.this, Binding_mobile_phone_page.class);
+                startActivity(intent);
+            }
+        });
+
+        personal_information_changes_page_name_relativelayout = (RelativeLayout) findViewById(R.id.personal_information_changes_page_name_relativelayout);
+
+        personal_information_changes_page_name_relativelayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Personal_information_changes_page.this, Personal_settings_name_change_page.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/chen/myapplication/Personal_settings_sex_change_page.java
@@ -0,0 +1,15 @@
+package com.example.chen.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chen on 2016/7/9.
+ */
+public class Personal_settings_sex_change_page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_settings_sex_change_page);
+    }
+}
