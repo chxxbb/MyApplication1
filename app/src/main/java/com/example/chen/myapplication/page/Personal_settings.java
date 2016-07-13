3a975961e3b4b9007680308ff5b78be90eace9b3
chxxbb <chxxbb@outlook.com>
2016-07-13 14:39:17 +0800

    2016年7月13日 14:39:03
               新增:
               1.设置页面和跳转逻辑

--- app/src/main/java/com/example/chen/myapplication/page/Personal_settings.java
+++ app/src/main/java/com/example/chen/myapplication/page/Personal_settings.java
@@ -11,6 +11,7 @@ import android.widget.RelativeLayout;
 
 import com.example.chen.myapplication.R;
 import com.example.chen.myapplication.view.Personal_information_changes_page;
+import com.example.chen.myapplication.view.Setting;
 
 /**
  * Created by Chen on 2016/7/7.
@@ -18,6 +19,7 @@ import com.example.chen.myapplication.view.Personal_information_changes_page;
 public class Personal_settings extends Fragment {
 
     RelativeLayout personal_settings_basic_information_relativelayout = null;
+    RelativeLayout personal_settings_setting_relativelayout = null;
 
 
     @Nullable
@@ -32,6 +34,15 @@ public class Personal_settings extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), Personal_information_changes_page.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+        personal_settings_setting_relativelayout = (RelativeLayout) view.findViewById(R.id.personal_settings_setting_relativelayout);
+        personal_settings_setting_relativelayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Setting.class);
                 getActivity().startActivity(intent);
             }
         });
