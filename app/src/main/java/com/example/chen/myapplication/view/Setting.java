f988b7224a44b2dd3e2a8b7e09d65a8d28fa5f12
chxxbb <chxxbb@outlook.com>
2016-07-13 15:29:07 +0800

    2016年7月13日 15:29:03
               新增:
               1.更新页面

--- app/src/main/java/com/example/chen/myapplication/view/Setting.java
+++ app/src/main/java/com/example/chen/myapplication/view/Setting.java
@@ -1,7 +1,10 @@
 package com.example.chen.myapplication.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.chen.myapplication.R;
 
@@ -9,9 +12,22 @@ import com.example.chen.myapplication.R;
  * Created by Chen on 2016/7/13.
  */
 public class Setting extends Activity {
+
+    RelativeLayout setting_update = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+
+        setting_update = (RelativeLayout) findViewById(R.id.setting_update);
+        setting_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Setting.this, Setting_update_page.class);
+                Setting.this.startActivity(intent);
+            }
+        });
+
     }
 }
