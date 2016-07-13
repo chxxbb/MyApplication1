d1dceb09c3a77a57ee0b611813c57f41b5a4ab2c
chxxbb <chxxbb@outlook.com>
2016-07-13 10:24:39 +0800

    2016年7月13日 10:24:22
               新增:
               1.上传头像页面的菜单栏

--- app/src/main/java/com/example/chen/myapplication/view/Personal_settings_head_portrait_change_page.java
+++ app/src/main/java/com/example/chen/myapplication/view/Personal_settings_head_portrait_change_page.java
@@ -3,6 +3,7 @@ package com.example.chen.myapplication.view;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -27,7 +28,6 @@ public class Personal_settings_head_portrait_change_page extends Activity {
             @Override
             public void onClick(View v) {
                 Personal_settings_head_portrait_change_page.this.openOptionsMenu();
-                Toast.makeText(Personal_settings_head_portrait_change_page.this, "sss", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -36,38 +36,27 @@ public class Personal_settings_head_portrait_change_page extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        menu.add(Menu.NONE, Menu.FIRST + 1, 5, "删除").setIcon(
+        menu.add(Menu.NONE, Menu.FIRST + 1, 1, "选择图片");
 
-                android.R.drawable.ic_menu_delete);
+        menu.add(Menu.NONE, Menu.FIRST + 2, 2, "取消");
 
-        // setIcon()方法为菜单设置图标，这里使用的是系统自带的图标，同学们留意一下,以
-
-        // android.R开头的资源是系统提供的，我们自己提供的资源是以R开头的
-
-        menu.add(Menu.NONE, Menu.FIRST + 2, 2, "保存").setIcon(
-
-                android.R.drawable.ic_menu_edit);
-
-        menu.add(Menu.NONE, Menu.FIRST + 3, 6, "帮助").setIcon(
-
-                android.R.drawable.ic_menu_help);
-
-        menu.add(Menu.NONE, Menu.FIRST + 4, 1, "添加").setIcon(
-
-                android.R.drawable.ic_menu_add);
-
-        menu.add(Menu.NONE, Menu.FIRST + 5, 4, "详细").setIcon(
-
-                android.R.drawable.ic_menu_info_details);
-
-        menu.add(Menu.NONE, Menu.FIRST + 6, 3, "发送").setIcon(
-
-                android.R.drawable.ic_menu_send);
-
-        return true;
-
+        return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        switch (item.getItemId()) {
+            case Menu.FIRST + 1:
+                Toast.makeText(this, "ss", Toast.LENGTH_LONG).show();
+                break;
+            case Menu.FIRST + 2:
+                Toast.makeText(this, "ddd", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
 
