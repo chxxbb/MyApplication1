378ee1f1d0267e1f36454e2ef6599707344581da
chxxbb <chxxbb@outlook.com>
2016-07-14 14:35:07 +0800

    2016年7月14日 14:34:56
               新增:
               1.预约详情

---
+++ app/src/main/java/com/example/chen/myapplication/view/My_reservation_page.java
@@ -0,0 +1,17 @@
+package com.example.chen.myapplication.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.chen.myapplication.R;
+
+/**
+ * Created by Chen on 2016/7/14.
+ */
+public class My_reservation_page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_reservation_page);
+    }
+}
